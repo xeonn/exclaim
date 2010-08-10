63449593efa4f1d5c33391b9f1941048377e52a8
Onn Khairuddin Ismail <onn.khairuddin@gmail.com>
2010-08-11 01:49:39 +0800

    Check existence of User information and force user to fill it up.

--- main.cpp
+++ main.cpp
@@ -2,12 +2,34 @@
 #include <QModelIndex>
 #include "mainwindow.h"
 
+#include <QSqlQuery>
+#include <QSqlRecord>
+#include <QMessageBox>
+
+#include "settings.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
 
     w.initData();
+
+    // Check for existence of user information. Cannot be blank.
+
+    QSqlQuery qry;
+    qry.exec("select name, jobtitle, department from c_userinfo");
+    qry.next();
+    if( qry.record().value("name").isNull() )
+    {
+        QMessageBox bx;
+        bx.setText("Your Name, Job Title information is missing.\nPlease update.");
+        bx.exec();
+
+        Settings dlg;
+        if(!dlg.exec())
+            return 0;
+    }
 
     w.show();
     return a.exec();
